a8ea4d9a50ccee163e0e3cca730ff2941fb3b3c7
lupinglade <lupinglade@cyaneous.com>
2025-02-12 13:50:34 -0800

    Sizing work

--- src/cards/base-card.ts
+++ src/cards/base-card.ts
@@ -29,11 +29,9 @@ export class BaseCard extends LitElement {
   getLayoutOptions() {
     return {
       grid_columns: 2.5,
-      grid_min_columns: 1,
-      // grid_max_columns: 2,
+      grid_min_columns: 2,
       grid_rows: 4,
       grid_min_rows: 3,
-      // grid_max_rows: 3,
     };
   }
 
